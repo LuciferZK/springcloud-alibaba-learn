ec9afe8d24580af36e8b94c81ec6aa16197273bd
LuciferZK <13871278164@163.com>
2020-02-21 11:56:50 +0800

    1.sentinel开启链路限流

--- shop-order/src/main/java/com/lucifer/controller/OderController3.java
+++ shop-order/src/main/java/com/lucifer/controller/OderController3.java
@@ -1,23 +1,31 @@
 package com.lucifer.controller;
 
+import com.lucifer.service.impl.OrderServiceImpl3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 
 @RestController
 @Slf4j
 public class OderController3 {
 
+    @Resource
+    private OrderServiceImpl3 orderServiceImpl3;
+
     //测试高并发
     @GetMapping(value = "/order/message1")
     public String message() {
+        orderServiceImpl3.message();
         return "message1";
     }
 
     //测试高并发
     @GetMapping(value = "/order/message2")
     public String message2() {
+        orderServiceImpl3.message();
         return "message2";
     }
 }
