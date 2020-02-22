5f343c7354d0194f4cd247907791b79d0d40c1fd
LuciferZK <13871278164@163.com>
2020-02-22 12:12:12 +0800

    sentinel提供的gateway适配模块sentinel-spring-cloud-gateway-adapter，1：实现利用route维度限流（routeId）

---
+++ api-gateway/src/main/java/com/lucifer/config/GatewayConfiguration.java
@@ -0,0 +1,65 @@
+package com.lucifer.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 基于sentinel的gateway限流是通过filter来完成的，使用时只需要注入sentinelGatewayFilter实例以及
+ * SentinelGatewayBlockExceptionHandler实例即可。
+ */
+@Configuration
+public class GatewayConfiguration {
+
+    private final List<ViewResolver> viewResolvers;
+
+    private final ServerCodecConfigurer serverCodecConfigurer;
+
+    public GatewayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider, ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+
+    //初始化一个限流的过滤器
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }
+
+    //配置初始化的限流参数
+    @PostConstruct
+    public void initGatewayRules() {
+        Set<GatewayFlowRule> rules = new HashSet<>();
+        rules.add(
+                new GatewayFlowRule("product_route") //资源名称，对应路由id
+                        .setCount(1) //限流阈值
+                        .setIntervalSec(1) //统计时间窗口，单位是秒，默认是1秒
+        );
+        GatewayRuleManager.loadRules(rules);
+    }
+
+    //配置限流的异常处理器
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+
+
+}
